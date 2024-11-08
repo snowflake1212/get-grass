7e8d924b4ba6ed1558cff98460a4393fa8822443
snowflake1212 <76563139+snowflake1212@users.noreply.github.com>
2024-11-08 22:54:32 +0700

    Update 24run.sh

--- 24run.sh
+++ 24run.sh
@@ -3,10 +3,13 @@ echo "Changing nameserver..."
 echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
 
 # Restart Grass Node
-pkill -f random_gtvpn.py
+pkill -f random_gtvpn.py &&
 echo "Killing openvpn..."
 killall openvpn
-sleep 10
+sleep 5
+rm -f random_gtvpn.py &&
+wget https://raw.githubusercontent.com/snowflake1212/get-grass/refs/heads/main/random_gtvpn.py &&
+chmod +x random_gtvpn.py
 python3 random_gtvpn.py JP KR US RO TH VN BR MY QA RU ZZ ID AR &
 sleep 10
 
